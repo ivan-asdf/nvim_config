8063000a82a369791acf485fed31ff566f2eeddf
Ivan <ivan.asdf0@gmail.com>
2022-06-28 12:22:02 +0300

    Added packer and nvim_tree.

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 require('options')
 require('maps')
+require('plugins')
+
+-- examples for your init.lua
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+require('nvim-web-devicons').setup()
+
+-- vim.g.nvim_tree_show_icons = {git = 1, folders =  0, files = 0, folder_arrows = 0}
---
+++ lua/plugins.lua
@@ -0,0 +1,10 @@
+return require('packer').startup(function()
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icons
+		},
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	}
+
+end)
